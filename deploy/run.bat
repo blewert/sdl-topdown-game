9bfec2ce4452c837cf158ac659566ff66c97e63a
blewert <trewelb@gmail.com>
2026-04-25 00:30:22 +0100

    Added deployment script for mass download
    Adds simple batch file with caffeine

---
+++ deploy/run.bat
@@ -0,0 +1,32 @@
+@echo off
+
+timeout /t 3
+
+echo Opening caffeine
+start \\silo\student\games\caffeine\caffeine64.exe
+
+echo Cloning repository
+git clone https://github.com/StaffsUniGames/sdl-topdown-game.git
+cd sdl-topdown-game
+
+echo Opening instructions
+start "" https://github.com/StaffsUniGames/sdl-topdown-game/blob/main/README.md
+
+echo Opening project
+start ./sdl-game-activity.sln
+
+timeout /t 5
+
+echo.
+echo.
+echo.
+
+echo Checking processes running (no output = not running):
+
+echo .. Caffiene:
+tasklist | find "caff"
+
+echo .. :
+tasklist | find "caff"
+
+timeout /t 5
